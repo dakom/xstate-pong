d2d19c48618f48f7685b29f03ba94829924fe57c
David Komer <david.komer@gmail.com>
2019-01-20 16:09:34 +0200

    update pong-renderer

--- src/Main.ts
+++ src/Main.ts
@@ -1,27 +1,42 @@
 import {buildVersion, isProduction} from "utils/config/Config";
-import {setup, CollisionName, ControllerValue} from "pong-renderer";
+import {setup, CollisionName} from "pong-renderer";
 
 console.log(`%c XState-Pong ${buildVersion} (productionMode: ${isProduction})`, 'color: #4286f4; font-size: large; font-family: "Comic Sans MS", cursive, sans-serif');
 
-let _onCollision; //Just for demo purposes
-const handleController = (value:ControllerValue) => {
-    if(value === ControllerValue.UP) {
-        //real-world would move paddle
-        _onCollision(CollisionName.LEFT_WALL);
-    }
-}
 
-setup({ handleController}).then(({constants, onRender, onCollision}) => {
-    const {canvasWidth, canvasHeight, ballRadius, paddleWidth, paddleHeight} = constants;
+setup().then(({constants, render: _render, addPoint, playCollisionAudio}) => {
+    const {canvasWidth, canvasHeight, paddleWidth} = constants;
 
     //Render the items in the middle of the screen (without helper)
-    onRender({
+    //Real-world would have this in a requestAnimationFrame cycle
+    //With the dynamic object positions
+    const render = () => _render({
         ball: {x: canvasWidth/2, y: canvasHeight/2},
         paddle1: {x: paddleWidth/2, y: canvasHeight/2},
         paddle2: {x: canvasWidth - (paddleWidth/2), y: canvasHeight/2},
     });
 
-    //Can't call it right away in some browsers
-    //so deferred until user presses up
-    _onCollision = onCollision;
+    //This would be called based on physics
+    //We could decide to have a different audio player or use the default 
+    const handleCollision = (collisionName:CollisionName) => {
+        if(collisionName === CollisionName.LEFT_WALL) {
+            addPoint(2);
+        } else if(collisionName === CollisionName.RIGHT_WALL) {
+            addPoint(1);
+        } 
+
+        playCollisionAudio(collisionName);
+
+        //re-render to show updated score
+        render();
+    }
+
+    //Just to give the idea - pretend the ball hit the left wall on click
+    //Some browsers requires a user gesture before playing audio
+    window.onclick = () => handleCollision(CollisionName.LEFT_WALL);
+
+    //Render the first screen
+    render();
 });
+
+
